f56f3928b31c996a7f69123ccb9eff5292dc40c2
NotKachori <notkachori@gmail.com>
2026-09-07 20:01:55 +0530

    added intro ( not completed)

---
+++ Scripts/intro.gd
@@ -0,0 +1,35 @@
+extends Control
+
+@onready var text_label: Label = $ColorRect/Label
+@onready var next_button: Button = $Button
+
+var text_index := 0
+
+var texts = [
+	"I was just drinking with my crew...",
+	"Ugh... my head hurts.",
+	"Where the hell am I?",
+	"Where's my crew?",
+	"...At least I still have my drink and my weapons."
+]
+
+
+func _ready():
+	text_label.text = texts[text_index]
+	next_button.text = "NEXT"
+
+
+func _on_button_pressed() -> void:
+
+	if text_index < texts.size() - 1:
+		text_index += 1
+		text_label.text = texts[text_index]
+
+	else:
+		next_button.text = "WAKE UP"
+		next_button.pressed.disconnect(_on_button_pressed)
+		next_button.pressed.connect(_wake_up)
+
+
+func _wake_up():
+	get_tree().change_scene_to_file("res://Scenes/cave.tscn")
